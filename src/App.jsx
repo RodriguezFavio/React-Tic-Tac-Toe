11f792897ad95e548571af4b54987f124efdf6de
RodriguezFavio <favio.rodriguez.flores@gmail.com>
2024-09-30 16:47:03 -0300

    Add all logic and component in the app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,84 @@
+import { useState } from 'react';
+
+import Player from './components/Player/Player';
+import GameBoard from './components/GameBoard/GameBoard';
+import Log from './components/Log/Log';
+import GameOver from './components/GameOver/GameOver';
+
+import deriveActivePlayer from './utils/deriveActivePlayer';
+import deriveGameBoard from './utils/deriveGameBoard';
+import deriveWinner from './utils/deriveWinner';
+
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2',
+};
+
 function App() {
-  return <h1>React Tic-Tac-Toe</h1>;
+  const [players, setPlayers] = useState(PLAYERS);
+
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, players);
+  const hasDraw = gameTurns.length === 9 && !winner;
+
+  function handleSelectSqueare(rowIndex, columnIndex) {
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
+
+      const updatedTurns = [
+        {
+          square: { row: rowIndex, column: columnIndex },
+          player: currentPlayer,
+        },
+        ...prevTurns,
+      ];
+      return updatedTurns;
+    });
+  }
+
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName,
+      };
+    });
+  }
+
+  return (
+    <>
+      <main>
+        <div id="game-container">
+          <ol id="players" className="highlight-player">
+            <Player
+              initialName={PLAYERS.X}
+              symbol="X"
+              isActive={activePlayer === 'X'}
+              onChangeName={handlePlayerNameChange}
+            />
+            <Player
+              initialName={PLAYERS.O}
+              symbol="O"
+              isActive={activePlayer === 'O'}
+              onChangeName={handlePlayerNameChange}
+            />
+          </ol>
+          {(winner || hasDraw) && (
+            <GameOver winner={winner} onRestart={handleRestart} />
+          )}
+          <GameBoard onSelectSquare={handleSelectSqueare} board={gameBoard} />
+        </div>
+        <Log turns={gameTurns} players={players} />
+      </main>
+    </>
+  );
 }
 
 export default App;
