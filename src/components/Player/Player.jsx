1cd3b536a1b2e3b75bf6a9f06e4db2e86130e7e3
RodriguezFavio <favio.rodriguez.flores@gmail.com>
2024-09-30 13:07:33 -0300

    Created player component

---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import './Player.css';
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((isEditing) => !isEditing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li className={isActive ? 'active' : undefined}>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            type="text"
+            value={playerName}
+            onChange={handleChange}
+            required
+          ></input>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{!isEditing ? 'Edit' : 'Save'}</button>
+    </li>
+  );
+}
